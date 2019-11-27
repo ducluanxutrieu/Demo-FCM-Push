e2c408e2f9b580c74af6a3a714a42445789d6cb5
ducluanxutrieu <ducluanxutrieu@gmail.com>
2019-11-27 21:19:11 +0700

    First commit

---
+++ app/src/main/java/edu/uit/testfcmfirebase/MainActivity.java
@@ -0,0 +1,47 @@
+package edu.uit.testfcmfirebase;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainTag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getDeviceToken();
+    }
+
+    private void getDeviceToken() {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = Objects.requireNonNull(task.getResult()).getToken();
+
+                        // Log and toast
+                        Log.d(TAG, token);
+                    }
+                });
+
+    }
+}
